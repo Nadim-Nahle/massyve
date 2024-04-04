ff9a53509df21d06212e5c44ee6f0873a8c6b961
Nadim-Nahle <nahlenadim@gmail.com>
2024-04-04 10:24:35 +0300

    Setup fastify

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,20 @@
 const fastify = require("fastify");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const port = process.env.PORT;
 require("dotenv/config");
+
+const userRoutes = require("./routes/userRoutes");
+
+fastify.register(userRoutes, { prefix: "/api/v1/users" });
+
+const start = async () => {
+  try {
+    await fastify.listen(port || 5000);
+    fastify.log.info(`server started on port ${fastify.server.address().port}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
