969bc6cf97f7b9fdfa1089582680571ce19d2f88
Nadim-Nahle <nahlenadim@gmail.com>
2024-04-04 14:02:49 +0300

    Connect db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,18 @@
-const fastify = require("fastify");
+const fastify = require("fastify")();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT;
+const DB_CONNECTION = process.env.DB_CONNECTION;
 require("dotenv/config");
 
 const userRoutes = require("./routes/userRoutes");
+
+mongoose.connect(process.env.MONGO_URI,{
+useNewUrlParser: true,
+useUnifiedTopology: true,  
+})
+.then(() => console.log("connected to db"))
+.catch((e) => console.log("error connecting to db", e))
 
 fastify.register(userRoutes, { prefix: "/api/v1/users" });
 
