df8ec43f2dde222a624fc5c552a444b8f9218e5e
Nadim-Nahle <nahlenadim@gmail.com>
2024-04-04 14:39:32 +0300

    Add auth middleware to routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const userController = require("../controllers/userController");
+const auth = require("../middlewares/AuthMiddleware");
 
 async function routes(fastify, options){
     fastify.get("/", userController.getUsers);
     fastify.get("/:id", userController.getUserById);
     fastify.post("/", userController.createUser);
-    fastify.put("/:id", userController.updateUser);
-    fastify.delete("/:id", userController.deleteUser);
+    fastify.put("/:id", { preHandler: auth }, userController.updateUser);
+    fastify.delete("/:id",{ preHandler: auth }, userController.deleteUser);
+    fastify.post("/login", userController.login);
 }
 
 module.exports = routes;
