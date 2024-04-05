83ee94276d3f6715b6d8f9e86e0b07857887c051
nadim nahle <nahlenadim@gmail.com>
2024-04-05 11:20:58 +0300

    Update fastify version

--- backend/app.js
+++ backend/app.js
@@ -6,16 +6,11 @@ require("dotenv/config");
 
 const userRoutes = require("./src/routes/userRoutes");
 
-mongoose.connect(process.env.MONGO_URI,{
-useNewUrlParser: true,
-useUnifiedTopology: true,  
-})
+mongoose.connect(process.env.MONGO_URI,{})
 .then(() => console.log("connected to db"))
 .catch((e) => console.log("error connecting to db", e))
 
-fastify.register(require('@fastify/cors'), {
-  origin: '*'
-});
+
 fastify.register(userRoutes, { prefix: "/api/v1/users" });
 
 const start = async () => {
